50e27986a40435568fa126d2b0ff481de55ed0f9
Jan-Torsten Milde <milde@hs-fulda.de>
2025-03-10 14:51:09 +0100

    Add files via upload

--- demos/kokoro/do_speak.py
+++ demos/kokoro/do_speak.py
@@ -28,7 +28,7 @@ generator = pipeline(
 )
 
 # Alternatively, load voice tensor directly:
-voice_tensor = torch.load('path/to/voice.pt', weights_only=True)
+voice_tensor = torch.load('./models/af_bella.pt', weights_only=True)
 generator = pipeline(
     text, voice=voice_tensor,
     speed=1, split_pattern=r'\n+'
@@ -38,5 +38,5 @@ for i, (gs, ps, audio) in enumerate(generator):
     print(i)  # i => index
     print(gs) # gs => graphemes/text
     print(ps) # ps => phonemes
-    display(Audio(data=audio, rate=24000, autoplay=i==0))
+    #display(Audio(data=audio, rate=24000, autoplay=i==0))
     sf.write(f'{i}.wav', audio, 24000) # save each audio file
